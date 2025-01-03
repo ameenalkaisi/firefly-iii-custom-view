1dcfe72bfb03fe9d145592290751dd41903e3f29
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2025-01-03 18:03:39 -0500

    WIP on parser

--- src/utils/firefly-api/parsers.ts
+++ src/utils/firefly-api/parsers.ts
@@ -1,6 +1,22 @@
 import { Transaction } from "@/types/domain";
 import { components } from "@/types/firefly-api";
 
-export const getTransactionsFromResponse = (data: components["schemas"]["TransactionArray"]): Transaction[] => {
+export const getTransactionsFromResponse = (
+  transactionArrayContainer: components["schemas"]["TransactionArray"],
+): Transaction[] => {
+  const transactions = transactionArrayContainer.data
+    .flatMap((entry) => entry.attributes)
+    .flatMap((attribute) =>
+      attribute.transactions.map((transaction) => ({
+        user: attribute.user,
+        created_at: attribute.created_at,
+        updated_at: attribute.updated_at,
+        group_title: attribute.group_title,
+        transaction: transaction,
+      })),
+    );
+
+  console.log("TRANSACTIONS: ", transactions);
+
   return [];
 };
